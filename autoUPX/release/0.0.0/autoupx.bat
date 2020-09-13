cc7577f3161d4964f9ed241e5c558b65dfb13631
Blazzycrafter <blazzycrafter@ldiot.eu>
2020-09-13 05:16:55 +0200

    update 1

---
+++ autoUPX/release/0.0.0/autoupx.bat
@@ -0,0 +1,24 @@
+@echo% %off
+set 1=%1 & set 2=%2 & set 3=%3 & set 4=%4 & set 5=%5 & set 6=%6 & set 7=%7 & set 8=%8 & set 9=%9
+shift
+set 10=%9
+shift & shift & shift & shift & shift & shift & shift & shift & shift
+set 11=%1 &  set 12=%2 &  set 13=%3 &  set 14=%4 &  set 15=%5 &  set 16=%6 &  set 17=%7 &  set 18=%8 &  set 19=%9
+shift
+set 20=%9
+shift & shift & shift & shift & shift & shift & shift & shift & shift
+set 21=%1 &  set 22=%2 &  set 23=%3 &  set 24=%4 &  set 25=%5 &  set 26=%6 &  set 27=%7 &  set 28=%8 &  set 29=%9
+shift
+set 30=%9
+shift & shift & shift & shift & shift & shift & shift & shift & shift
+set 31=%1 &  set 32=%2 &  set 33=%3 &  set 34=%4 &  set 35=%5 &  set 36=%6 &  set 37=%7 &  set 38=%8 &  set 39=%9
+shift
+set 40=%9
+shift & shift & shift & shift & shift & shift & shift & shift & shift
+set 1=%1 & set 2=%2 & set 3=%3 & set 4=%4 & set 5=%5 & set 6=%6 & set 7=%7 & set 8=%8 & set 9=%9
+shift
+set 50=%9
+
+
+
+upx %1% %2% %3% %4% %5% %6% %7% %8% %9% %10% %11% %12% %13% %14% %15% %16% %17% %18% %19% %20% %21% %22% %23% %24% %25% %26% %27% %28% %29% %30% %31% %32% %33% %34% %35% %36% %37% %38% %39% %40% %41% %42% %43% %44% %45% %46% %47% %48% %49% %50%
